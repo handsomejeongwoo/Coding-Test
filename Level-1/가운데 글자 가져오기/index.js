f181eb445083059e9890d0cff8f2f850c257088d
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-11-29 09:11:34 +0900

    code refactoring

--- Level-1/가운데 글자 가져오기/index.js
+++ Level-1/가운데 글자 가져오기/index.js
@@ -1,9 +1,9 @@
 function solution(s) {
+  var answer = "";
   var mok = Math.floor([...s].length / 2);
 
   if (s.length % 2 === 0) {
-    answer += [...s][mok - 1];
-    answer += [...s][mok];
+    answer += [...s][mok - 1] += [...s][mok];
   } else {
     answer += [...s][mok];
   }
